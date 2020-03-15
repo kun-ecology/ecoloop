e0acbe6a100f9170486f7b1c6dcf473878165d74
tequierogk <mlngm@hotmail.com>
2020-03-15 15:12:45 +0100

    get_lm_summary support sarlm model

--- R/get_lm_summary.R
+++ R/get_lm_summary.R
@@ -1,4 +1,4 @@
-#' Get tidy results from linear, nls, lmer, lme mode
+#' Get tidy results from linear, nls, lmer, lme,  sarlm mode
 #'
 #' @param lm.mod a model returned by function lm, nls, lmer, lme
 #' @param CI whether return confidence interval for the estimates, only works for lm model
@@ -15,10 +15,10 @@ get_lm_summary <- function(lm.mod,CI=NULL,k=NULL){
     trimws(format(round(x, k) %>% try(silent = T), nsmall=k))
   }
   if (class(lm.mod)=="try-error"){
-    lm.summary.res <- rep(NA,9)
+    lm.summary.res <- rep(NA,10)
     lm.summary.res <- t(lm.summary.res) %>% as.data.frame()
     colnames(lm.summary.res) <- c("Vars", "Estimate", "Std.Error", "t.value",
-                                  "P", "R2", "AdjR2", "lm.P", "AIC")
+                                  "P", "R2", "AdjR2", "lm.P", "AIC", "BIC")
     return(lm.summary.res)
   } else if (class(lm.mod)=="lm") {
 
@@ -31,6 +31,13 @@ get_lm_summary <- function(lm.mod,CI=NULL,k=NULL){
                               lm.res$fstatistic[3],
                               lower.tail=FALSE)
     lm.summary.res$AIC <- AIC(lm.mod)
+    lm.summary.res$BIC <- BIC(lm.mod)
+    lm.summary.res <- mutate(lm.summary.res,Vars=row.names(lm.summary.res))
+    lm.summary.res <- mutate_if(lm.summary.res,is.numeric,specify_decimal)
+    lm.summary.res <- mutate_at(lm.summary.res,1:8,function(x)suppressWarnings(as.numeric(x)))
+    lm.summary.res <- lm.summary.res[,c(10,1:9)]
+    names(lm.summary.res)[3:5] <- c("Std.Error","t.value","P")
+
   } else if (class(lm.mod)=="nls"){
     lm.res <- summary(lm.mod)
     lm.summary.res <- lm.res$coefficients %>% as.data.frame()
@@ -38,15 +45,27 @@ get_lm_summary <- function(lm.mod,CI=NULL,k=NULL){
     lm.summary.res$AdjR2<- NA
     lm.summary.res$lm.P <- NA
     lm.summary.res$AIC <- AIC(lm.mod)
+    lm.summary.res$BIC <- BIC(lm.mod)
+    lm.summary.res <- mutate(lm.summary.res,Vars=row.names(lm.summary.res))
+    lm.summary.res <- mutate_if(lm.summary.res,is.numeric,specify_decimal)
+    lm.summary.res <- mutate_at(lm.summary.res,1:8,function(x)suppressWarnings(as.numeric(x)))
+    lm.summary.res <- lm.summary.res[,c(10,1:9)]
+    names(lm.summary.res)[3:5] <- c("Std.Error","t.value","P")
 
   } else if (class(lm.mod)=="lme"){
     lm.res <- summary(lm.mod)
     lm.summary.res <- lm.res$tTable %>% as.data.frame()
     lm.summary.res <- lm.summary.res[,c(1,2,4,5)]
-    lm.summary.res$R2 <- NA # do not why can not get R2 with r.squaredGLMM function
+    lm.summary.res$R2 <- NA # do not know why can not get R2 with r.squaredGLMM function
     lm.summary.res$AdjR2<- NA
     lm.summary.res$lm.P <- NA
     lm.summary.res$AIC <- AIC(lm.mod)
+    lm.summary.res$BIC <- BIC(lm.mod)
+    lm.summary.res <- mutate(lm.summary.res,Vars=row.names(lm.summary.res))
+    lm.summary.res <- mutate_if(lm.summary.res,is.numeric,specify_decimal)
+    lm.summary.res <- mutate_at(lm.summary.res,1:8,function(x)suppressWarnings(as.numeric(x)))
+    lm.summary.res <- lm.summary.res[,c(10,1:9)]
+    names(lm.summary.res)[3:5] <- c("Std.Error","t.value","P")
   } else if (class(lm.mod)=="lmerMod"){
     lm.res <- summary(lm.mod)
     lm.summary.res <- lm.res$coefficients %>% as.data.frame()
@@ -56,12 +75,26 @@ get_lm_summary <- function(lm.mod,CI=NULL,k=NULL){
     lm.summary.res$AdjR2<- MuMIn::r.squaredGLMM(lm.mod)[1] #Marginal R_GLMM² represents the variance explained by the fixed effects
     lm.summary.res$lm.P <- NA # no such value for lmer package
     lm.summary.res$AIC <- AIC(lm.mod)
+    lm.summary.res$BIC <- BIC(lm.mod)
+    lm.summary.res <- mutate(lm.summary.res,Vars=row.names(lm.summary.res))
+    lm.summary.res <- mutate_if(lm.summary.res,is.numeric,specify_decimal)
+    lm.summary.res <- mutate_at(lm.summary.res,1:8,function(x)suppressWarnings(as.numeric(x)))
+    lm.summary.res <- lm.summary.res[,c(10,1:9)]
+    names(lm.summary.res)[3:5] <- c("Std.Error","t.value","P")
+  } else if (class(lm.mod)=="sarlm") {
+
+    lm.res <- summary(lm.mod, correlation=F, Nagelkerke=TRUE, Hausman=TRUE)
+    lm.summary.res <- lm.res$Coef %>%
+      as.data.frame() %>%
+      bind_cols(Vars=row.names(.),.)
+    lm.summary.res$R2 <- lm.res$NK
+    lm.summary.res$AIC <- AIC(lm.mod)
+    lm.summary.res$BIC <- BIC(lm.mod)
+    lm.summary.res <- mutate_if(lm.summary.res,is.numeric,specify_decimal)
   }
-  lm.summary.res <- mutate(lm.summary.res,Vars=row.names(lm.summary.res))
-  lm.summary.res <- mutate_if(lm.summary.res,is.numeric,specify_decimal)
-  lm.summary.res <- mutate_at(lm.summary.res,1:8,function(x)suppressWarnings(as.numeric(x)))
-  lm.summary.res <- lm.summary.res[,c(9,1:8)]
-  names(lm.summary.res)[3:5] <- c("Std.Error","t.value","P")
+
+
+
 
 
   # only for linear model
